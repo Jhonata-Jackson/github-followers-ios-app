201c060ab8a32c429bd5fc7bbaba72e7dccd5016
Jhonata Jackson <jhonata.jacksondev@gmail.com>
2025-06-04 00:54:19 -0300

    feat: :sparkles: upload inital project

---
+++ GHFollowers/GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritiesVC.swift
+//  GHFollowers
+//
+//  Created by UserTQI on 03/06/25.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
